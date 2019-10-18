2cca91ad2267c805bf0e96b5eb828837d4fcc123
niravassar <assarn@objectcomputing.com>
2019-10-18 10:00:41 -0500

    eachproperty for stadium example

--- complete/src/main/groovy/micronaut/configuration/groovy/MyController.groovy
+++ complete/src/main/groovy/micronaut/configuration/groovy/MyController.groovy
@@ -1,23 +1,32 @@
 package micronaut.configuration.groovy
 
-
 import groovy.transform.CompileStatic
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+
+import javax.annotation.Nullable
+import javax.inject.Named
 
 @CompileStatic
 @Controller("/my")
 class MyController {
 
     TeamConfiguration teamConfiguration
+    StadiumConfiguration stadiumConfiguration
 
-    MyController(TeamConfiguration teamConfiguration1) {
-        this.teamConfiguration = teamConfiguration1
+    MyController(@Nullable TeamConfiguration teamConfiguration, @Nullable @Named("pnc") StadiumConfiguration stadiumConfiguration) {
+        this.teamConfiguration = teamConfiguration
+        this.stadiumConfiguration = stadiumConfiguration
     }
 
     @Get("/team")
     HttpResponse<TeamConfiguration> team() {
         return HttpResponse.ok(this.teamConfiguration)
+    }
+
+    @Get("/stadium")
+    HttpResponse<StadiumConfiguration> stadium() {
+        return HttpResponse.ok(this.stadiumConfiguration)
     }
 }
